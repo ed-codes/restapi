62ee16b209b28a2e8593e2cdf49108360f4434e0
Eduard Fastovski <hi@ed.codes>
2021-05-09 10:30:35 +0200

    routes template and rest client

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -2,13 +2,28 @@ const express = require('express');
 const router = express.Router(); 
 
 // getting all
+router.get('/', (req, res) => {
+  res.send('hello world'); 
+})
 
 // getting one
+router.get('/:id', (req, res) => {
+  res.send(req.params.id);
+})
 
 // creating one 
+router.post('/', (req, res) => {
+  
+})
 
 // updating one
+router.patch('/:id', (req, res) => {
+  
+})
 
 // deleting one
+router.delete('/:id', (req, res) => {
+  
+})
 
 module.exports = router; 
